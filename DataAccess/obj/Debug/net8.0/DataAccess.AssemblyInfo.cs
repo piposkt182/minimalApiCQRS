d44ea632ac56dc78e11dec066db09b7bf3051211
piposkt182 <piposkt182@gmail.com>
2025-09-22 15:26:06 -0500

    fsdfg

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+414f05d13e3c49ecdfcd808cc943cac5d9a9bff6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15b5a2b5a256b28ee9bbeb48b64714baa8c0cb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
